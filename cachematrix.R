99fd7b14d92d6a36346ae58997c7c0a5b1b1ef41
David Thulson <david.thulson@gmail.com>
2014-06-19 19:40:43 -0500

    Implemented, finished

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Pair of functions to allow matrix solve results to be cached
+# For example:
+#
+#> m<-makeCacheMatrix()
+#> aMatrix <- matrix(c(1:4), 2,2)
+#> m$set(aMatrix)
+#> cacheSolve(m)
+#> cacheSolve(m)
+#
+# In that example, solve will only be called once (by the first call to
+#   cacheSolve.  The second call to cahceSolve will reuse the results from
+#   the first call.
 
-## Write a short comment describing this function
-
+# Construct the cache object that can be used with cacheSolve to cache the results
+#   of a call to solve
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(i) inverse <<- i
+    getInverse <- function() inverse
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+# Invert / solve the matrix stored in the cache object, using the cached
+#   results if available
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getInverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setInverse(i)
+    i
 }
